c350f6fdf2a5f854de6c0d8f92a1b2ae4bce0a52
Mohed <mccdama@gmail.com>
2021-11-28 20:44:06 +0100

    GET verb & handling

--- main/server.go
+++ main/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	h "Echo-go/handle"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -11,5 +12,9 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Salut! ")
 	})
+	// http://localhost:1323/users/Mohed
+	e.GET("/users/:id", h.GetUser)
+
+	// go run main/server.go --> Listening on http://localhost:1323
 	e.Logger.Fatal(e.Start(":1323"))
 }
